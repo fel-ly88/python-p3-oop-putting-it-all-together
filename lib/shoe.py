b5f575088b6ed2967dab98f9bb879a9c9f79391b
Your Name <your-email@example.com>
2025-08-11 21:05:46 +0300

    add shoe.py

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = None
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, brand):
+        if isinstance(brand, str):
+            self._brand = brand
+        else:
+            raise TypeError("Brand must be a string")
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
